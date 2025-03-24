7d5709dbe7c177da1d445040652280a07e488644
gthamza <hamzawarisgt@gmail.com>
2025-03-24 21:41:20 +0500

    setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Rootlayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2" />
+        <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+        <h2 className="text-primary-100">PrepWise</h2>
+      </nav>
+      {children}
+    </div>
+  );
+};
+
+export default Rootlayout;
